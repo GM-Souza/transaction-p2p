4238e0aff4f00650ca16fc1a14b71b87cf153261
Guilherme Souza <guilhermematos2313@gmail.com>
2025-09-20 16:10:08 -0300

    fix: TransactionRepository

--- src/main/java/com/transaction_p2p/repository/TransactionRepository.java
+++ src/main/java/com/transaction_p2p/repository/TransactionRepository.java
@@ -3,5 +3,5 @@ package com.transaction_p2p.repository;
 import com.transaction_p2p.domain.transaction.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public class TransactionRepository extends JpaRepository<Transaction, Long> {
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 }
